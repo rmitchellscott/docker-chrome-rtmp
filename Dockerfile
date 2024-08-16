4776f68524e0b4a2933d9282b0a82c1798116ed3
Mitchell Scott <10804314+rmitchellscott@users.noreply.github.com>
2024-08-16 13:41:35 -0600

    feat: Swap to icecast audio instead of pulseaudio from browser

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM debian:latest
-RUN apt-get update && apt-get install -y vim pulseaudio chromium xvfb ffmpeg
-RUN adduser root pulse-access
+RUN apt-get update && apt-get install -y vim chromium xvfb ffmpeg
 WORKDIR /app
 COPY entrypoint.sh .
 CMD ["bash", "entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -12,17 +12,13 @@ fi
 Xvfb :99 -screen 0 ${SCREEN_WIDTH}x${SCREEN_HEIGHT}x24 &
 export DISPLAY=:99
 
-# Configure audio system
-rm -rf /var/run/pulse /var/lib/pulse /root/.config/pulse
-pulseaudio -D --verbose --exit-idle-time=-1 --system --disallow-exit
-
 # Browser time
 chromium --kiosk --no-sandbox --window-size=${SCREEN_WIDTH},${SCREEN_HEIGHT} --window-position=0,0 --enable-features=OverlayScrollbar --autoplay-policy=no-user-gesture-required "$WEB_URL" &
 
 
 # Use ffmpeg to capture the display and stream it to an RTMP server
 ffmpeg -f x11grab -s ${SCREEN_WIDTH}x${SCREEN_HEIGHT} -draw_mouse 0 -i :99.0 \
-    -f pulse -i default \
-    -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p \
+    -i $ICE_URL \
+    -c:v libx264 -preset ${FFMPEG_PRESET:-veryfast} -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p \
     -c:a aac -b:a 128k -ac 2 \
     -f flv $RTMP_URL
